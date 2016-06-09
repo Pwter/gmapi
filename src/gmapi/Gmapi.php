ccc05b01bab83814cc69f18cef11d8741b6ba692
Pwter <dakcsi@gmail.com>
2016-06-09 18:40:53 +0200

    basic functions

--- src/gmapi/Gmapi.php
+++ src/gmapi/Gmapi.php
@@ -4,9 +4,26 @@ namespace pwter;
 
 class Gmapi
 {
-    public function hello()
+    private $width;
+    private $height;
+
+    public function __construct($options)
     {
-        echo 'Hello.';
+        foreach ($options as $key => $value) {
+            $this->$key = $value;
+        }
     }
-    
+    public function width($width)
+    {
+        $this->width = $width;
+    }
+    public function height($height)
+    {
+        $this->height = $height;
+    }
+    public function drawMap()
+    {
+        return '<div id="googleMap" style="width:'. $this->width .'px;height:'. $this->height .'px;"></div>';
+    }
+
 }
