4b9643344127ca736b044ec4ecfe34c7d7739c0d
pwter <dakcsi@gmail.com>
2016-06-09 20:27:34 +0200

    update readme

--- src/gmapi/Gmapi.php
+++ src/gmapi/Gmapi.php
@@ -5,10 +5,16 @@ namespace pwter;
 class Gmapi
 {
     private $properties;
+    private $markers;
 
     public function __construct($options)
     {
         $this->properties = $options;
+    }
+
+    public function addMarker()
+    {
+        $this->markers[] = 'myCenter';
     }
 
     public function drawMap()
@@ -16,8 +22,18 @@ class Gmapi
         $map = '<script src="http://maps.googleapis.com/maps/api/js"></script>'
               .'<div id="googleMap" style="width:'. (isset($this->properties['width']) ? $this->properties['width'] : 400) .'px;height:'. (isset($this->properties['height']) ? $this->properties['height'] : 300) .'px;"';
 
+        // add properties as data-tags for js
         foreach ($this->properties as $key => $value) {
             $map .= ' data-'. $key .'="' . $value . '"';
+        }
+
+        // add markers as data-tags for js
+        if (count($this->markers) > 0) {
+            $map .= ' data-markers=\'{"position":';
+            foreach ($this->markers as $value) {
+                $map .= '"'.$value.'"';
+            }
+            $map .= '}\'';
         }
 
         $map .= '></div>'
--- src/gmapi/assets/js/gmapi.js
+++ src/gmapi/assets/js/gmapi.js
@@ -1,12 +1,26 @@
 function initialize() {
     var lat = $('#googleMap').data('lat');
     var lng = $('#googleMap').data('lng');
+    var zoom = $('#googleMap').data('zoom');
+    var mapType = $('#googleMap').data('map-type');
+
     var mapProp = {
         center:new google.maps.LatLng(lat,lng),
-        zoom:5,
-        mapTypeId:google.maps.MapTypeId.ROADMAP
+        zoom:zoom,
+        mapTypeId:google.maps.MapTypeId[mapType]
     };
-      var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+    var m = $('#googleMap').data('markers');
+
+    var myCenter = new google.maps.LatLng(lat,lng);
+    var marker=new google.maps.Marker({
+        position:myCenter,
+      });
+
+    marker.setMap(map);
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
